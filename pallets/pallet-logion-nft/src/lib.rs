c0c10ea788b9de65482fa1d8ce5c04141ba07b55
Gerard Dethier <gerard@logion.network>
2021-04-02 13:45:04 +0200

    chore: align style with .editorconfig file.

--- pallets/pallet-logion-nft/src/lib.rs
+++ pallets/pallet-logion-nft/src/lib.rs
@@ -12,65 +12,65 @@ mod mock;
 mod tests;
 
 pub trait Config: frame_system::Config {
-    type Event: From<Event<Self>> + Into<<Self as frame_system::Config>::Event>;
+	type Event: From<Event<Self>> + Into<<Self as frame_system::Config>::Event>;
 }
 
 type AssetId = Vec<u8>;
 
 #[derive(Encode, Decode, Default, Clone, PartialEq, Eq, Debug)]
 pub struct Asset {
-    id: AssetId,
-    name: Vec<u8>,
+	id: AssetId,
+	name: Vec<u8>,
 }
 
 decl_storage! {
-    trait Store for Module<T: Config> as LogionNft {
+	trait Store for Module<T: Config> as LogionNft {
 
-        AssetById get(fn asset_by_id):
-            map hasher(blake2_128_concat) AssetId => Asset;
+		AssetById get(fn asset_by_id):
+			map hasher(blake2_128_concat) AssetId => Asset;
 
-        TokenByAccount get(fn token_by_account):
-            map hasher(blake2_128_concat) T::AccountId => Vec<AssetId>;
-    }
+		TokenByAccount get(fn token_by_account):
+			map hasher(blake2_128_concat) T::AccountId => Vec<AssetId>;
+	}
 }
 
 decl_event!(
-    pub enum Event<T>
-    where
-        AccountId = <T as frame_system::Config>::AccountId,
-    {
-        TokenIssued(AssetId, AccountId),
-    }
+	pub enum Event<T>
+	where
+		AccountId = <T as frame_system::Config>::AccountId,
+	{
+		TokenIssued(AssetId, AccountId),
+	}
 );
 
 decl_error! {
-    pub enum Error for Module<T: Config> {
-        NoneValue,
-        StorageOverflow,
-    }
+	pub enum Error for Module<T: Config> {
+		NoneValue,
+		StorageOverflow,
+	}
 }
 
 decl_module! {
-    pub struct Module<T: Config> for enum Call where origin: T::Origin {
-        type Error = Error<T>;
+	pub struct Module<T: Config> for enum Call where origin: T::Origin {
+		type Error = Error<T>;
 
-        fn deposit_event() = default;
+		fn deposit_event() = default;
 
-        #[weight = 10_000 + T::DbWeight::get().writes(1)]
-        pub fn issue_asset(origin, asset: Asset) -> dispatch::DispatchResult {
-            let issuer = ensure_signed(origin)?;
+		#[weight = 10_000 + T::DbWeight::get().writes(1)]
+		pub fn issue_asset(origin, asset: Asset) -> dispatch::DispatchResult {
+			let issuer = ensure_signed(origin)?;
 
-            let asset_id = asset.id.clone();
-            if AssetById::contains_key(&asset_id) {
-                Err(Error::<T>::StorageOverflow)?
-            } else {
-                AssetById::insert(&asset_id, asset);
+			let asset_id = asset.id.clone();
+			if AssetById::contains_key(&asset_id) {
+				Err(Error::<T>::StorageOverflow)?
+			} else {
+				AssetById::insert(&asset_id, asset);
 				let mut account_assets = TokenByAccount::<T>::get(&issuer).clone();
 				account_assets.push(asset_id.clone());
 				TokenByAccount::<T>::insert::<T::AccountId, Vec<AssetId>>(issuer.clone(), account_assets);
-                Self::deposit_event(RawEvent::TokenIssued(asset_id, issuer));
-                Ok(())
-            }
-        }
-    }
+				Self::deposit_event(RawEvent::TokenIssued(asset_id, issuer));
+				Ok(())
+			}
+		}
+	}
 }
--- pallets/pallet-logion-nft/src/tests.rs
+++ pallets/pallet-logion-nft/src/tests.rs
@@ -3,14 +3,14 @@ use frame_support::{assert_ok};
 
 #[test]
 fn asset_creation_works() {
-    new_test_ext().execute_with(|| {
-        let asset_id = vec![1, 2, 3];
-        let asset = pallet_logion_nft::Asset {
-            id: asset_id.clone(),
-            name: "asset".as_bytes().to_vec()
-        };
-        assert_ok!(LogionNft::issue_asset(Origin::signed(1), asset.clone()));
-        assert_eq!(LogionNft::asset_by_id(asset_id.clone()), asset);
+	new_test_ext().execute_with(|| {
+		let asset_id = vec![1, 2, 3];
+		let asset = pallet_logion_nft::Asset {
+			id: asset_id.clone(),
+			name: "asset".as_bytes().to_vec()
+		};
+		assert_ok!(LogionNft::issue_asset(Origin::signed(1), asset.clone()));
+		assert_eq!(LogionNft::asset_by_id(asset_id.clone()), asset);
 		assert!(LogionNft::token_by_account(1).contains(&asset_id));
-    });
+	});
 }
