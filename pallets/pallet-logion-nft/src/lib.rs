14ad4a2bd10eb42f7b2e533e71275008dab910ba
Gerard Dethier <gerard@logion.network>
2021-04-02 16:47:32 +0200

    feat: identify asset based on hashed data.

--- pallets/pallet-logion-nft/src/lib.rs
+++ pallets/pallet-logion-nft/src/lib.rs
@@ -4,6 +4,8 @@ use frame_support::codec::{Decode, Encode};
 use frame_support::traits::Vec;
 use frame_support::{decl_error, decl_event, decl_module, decl_storage, dispatch, traits::Get};
 use frame_system::ensure_signed;
+use sp_runtime::traits::Hash;
+use frame_support::traits::Box;
 
 #[cfg(test)]
 mod mock;
@@ -15,22 +17,21 @@ pub trait Config: frame_system::Config {
 	type Event: From<Event<Self>> + Into<<Self as frame_system::Config>::Event>;
 }
 
-type AssetId = Vec<u8>;
+type AssetName = Vec<u8>;
 
 #[derive(Encode, Decode, Default, Clone, PartialEq, Eq, Debug)]
 pub struct Asset {
-	id: AssetId,
-	name: Vec<u8>,
+	name: AssetName,
 }
 
 decl_storage! {
 	trait Store for Module<T: Config> as LogionNft {
 
 		AssetById get(fn asset_by_id):
-			map hasher(blake2_128_concat) AssetId => Asset;
+			map hasher(blake2_128_concat) T::Hash => Asset;
 
 		TokenByAccount get(fn token_by_account):
-			map hasher(blake2_128_concat) T::AccountId => Vec<AssetId>;
+			map hasher(blake2_128_concat) T::AccountId => Vec<T::Hash>;
 	}
 }
 
@@ -38,15 +39,15 @@ decl_event!(
 	pub enum Event<T>
 	where
 		AccountId = <T as frame_system::Config>::AccountId,
+		Hash = <T as frame_system::Config>::Hash,
 	{
-		TokenIssued(AssetId, AccountId),
+		TokenIssued(Hash, AccountId),
 	}
 );
 
 decl_error! {
 	pub enum Error for Module<T: Config> {
-		NoneValue,
-		StorageOverflow,
+		AssetAlreadyExists,
 	}
 }
 
@@ -60,17 +61,23 @@ decl_module! {
 		pub fn issue_asset(origin, asset: Asset) -> dispatch::DispatchResult {
 			let issuer = ensure_signed(origin)?;
 
-			let asset_id = asset.id.clone();
-			if AssetById::contains_key(&asset_id) {
-				Err(Error::<T>::StorageOverflow)?
+			let asset_id = T::Hashing::hash(&asset_data(&asset));
+			if AssetById::<T>::contains_key(&asset_id) {
+				Err(Error::<T>::AssetAlreadyExists)?
 			} else {
-				AssetById::insert(&asset_id, asset);
+				AssetById::<T>::insert::<T::Hash, Asset>(asset_id, asset);
 				let mut account_assets = TokenByAccount::<T>::get(&issuer).clone();
 				account_assets.push(asset_id.clone());
-				TokenByAccount::<T>::insert::<T::AccountId, Vec<AssetId>>(issuer.clone(), account_assets);
+				TokenByAccount::<T>::insert::<T::AccountId, Vec<T::Hash>>(issuer.clone(), account_assets);
 				Self::deposit_event(RawEvent::TokenIssued(asset_id, issuer));
 				Ok(())
 			}
 		}
 	}
+}
+
+pub fn asset_data(asset: &Asset) -> Box<Vec<u8>> {
+	let mut data = Box::new(Vec::new());
+	data.extend_from_slice(&asset.name[..]);
+	return data;
 }
--- pallets/pallet-logion-nft/src/tests.rs
+++ pallets/pallet-logion-nft/src/tests.rs
@@ -1,16 +1,17 @@
-use crate::{mock::*};
-use frame_support::{assert_ok};
+use crate::mock::*;
+use frame_support::assert_ok;
+use sp_runtime::traits::Hash;
 
 #[test]
 fn asset_creation_works() {
 	new_test_ext().execute_with(|| {
-		let asset_id = vec![1, 2, 3];
 		let asset = pallet_logion_nft::Asset {
-			id: asset_id.clone(),
 			name: "asset".as_bytes().to_vec()
 		};
+		let asset_data = pallet_logion_nft::asset_data(&asset);
+		let asset_id = <Test as frame_system::Config>::Hashing::hash(&asset_data);
 		assert_ok!(LogionNft::issue_asset(Origin::signed(1), asset.clone()));
-		assert_eq!(LogionNft::asset_by_id(asset_id.clone()), asset);
+		assert_eq!(LogionNft::asset_by_id(&asset_id), asset);
 		assert!(LogionNft::token_by_account(1).contains(&asset_id));
 	});
 }
