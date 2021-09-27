5d17c7618714e8e5b71dbc77ad8da25b2d914d73
Gerard Dethier <gerard@logion.network>
2021-09-27 15:23:18 +0200

    feat: add LOC metadata.
    logion-network/logion-internal#186

--- pallets/logion_loc/src/benchmarking.rs
+++ pallets/logion_loc/src/benchmarking.rs
@@ -3,14 +3,24 @@ use super::*;
 use frame_system::RawOrigin;
 use frame_benchmarking::{benchmarks, whitelisted_caller, impl_benchmark_test_suite};
 use sp_std::{vec, vec::Vec, boxed::Box};
+use frame_support::assert_ok;
 
-#[allow(unused)]
-use crate::Module as LogionLoc;
+use crate::Pallet as LogionLoc;
 
 benchmarks! {
 	create_loc {
 		let caller: T::AccountId = whitelisted_caller();
-	}: _(RawOrigin::Signed(caller), Default::default())
+	}: _(RawOrigin::Signed(caller), Default::default(), Default::default())
+
+	add_metadata {
+		let caller: T::AccountId = whitelisted_caller();
+		let loc_id = Default::default();
+		let item = MetadataItem {
+			name: vec![1u8, 2u8, 3u8],
+			value: vec![4u8, 5u8, 6u8],
+		};
+		assert_ok!(LogionLoc::<T>::create_loc(RawOrigin::Signed(caller.clone()).into(), loc_id, Default::default()));
+	}: _(RawOrigin::Signed(caller), loc_id, item)
 }
 
 impl_benchmark_test_suite!(
