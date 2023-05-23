ff98fdadbfebdea125c2c207f51e0bc1c9e91d24
Benoit Devos <benoit@logion.network>
2023-05-23 15:17:48 +0200

    feat: include items publication by requester/issuer; feat: include acknowledgement of items by LLO.
    logion-network/logion-internal#897

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -51,6 +51,7 @@ pub use sp_runtime::{Perbill, Permill};
 // Additional imports
 use frame_system::EnsureRoot;
 use logion_shared::{CreateRecoveryCallFactory, MultisigApproveAsMultiCallFactory, MultisigAsMultiCallFactory, DistributionKey, LegalFee, EuroCent};
+use pallet_logion_loc::migrations::v13::AddAcknowledgeItems;
 use pallet_logion_loc::LocType;
 use pallet_multisig::Timepoint;
 
@@ -118,7 +119,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	//   `spec_version`, and `authoring_version` are the same between Wasm and native.
 	// This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
 	//   the compatible custom types.
-	spec_version: 142,
+	spec_version: 143,
 	impl_version: 2,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 5,
@@ -694,7 +695,7 @@ pub type Executive = frame_executive::Executive<
 	frame_system::ChainContext<Runtime>,
 	Runtime,
 	AllPalletsWithSystem,
-	(),
+	AddAcknowledgeItems<Runtime>,
 >;
 
 #[cfg(feature = "runtime-benchmarks")]
